0a6a50c6c41d3dd96368b2a384565f1a72f07cc0
melkonyan <melkonyan@tionit.com>
2020-10-19 16:14:48 +0300

    Add Admin and portal blazr

--- Tionit.MyShop/Tionit.MyShop.Portal/wwwroot/js/content-animation.js
+++
@@ -1,7 +0,0 @@
-﻿window.animateContent = function(){
-    var animation = "animate-fade-in-up";
-    $("#kt_content")
-        .one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-            e => $("#kt_content").removeClass(animation))
-        .removeClass(animation).addClass(animation);
-}
